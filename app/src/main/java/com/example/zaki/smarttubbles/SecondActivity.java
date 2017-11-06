58d78cf280a8ad7e24bb88392b869817e93d9201
zakimuhammad <zakimuhammad20@gmail.com>
2017-11-06 11:55:42 +0700

    tambahan

--- app/src/main/java/com/example/zaki/smarttubbles/SecondActivity.java
+++ app/src/main/java/com/example/zaki/smarttubbles/SecondActivity.java
@@ -1,13 +1,14 @@
 package com.example.zaki.smarttubbles;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        setContentView(R.layout.activity_service);
     }
+
 }
