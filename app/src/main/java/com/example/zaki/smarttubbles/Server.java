554ef9f084c0816800e232ad4135606a24041ba1
zakimuhammad <zakimuhammad20@gmail.com>
2017-11-15 18:55:53 +0700

    coba konek database

---
+++ app/src/main/java/com/example/zaki/smarttubbles/Server.java
@@ -0,0 +1,8 @@
+package com.example.zaki.smarttubbles;
+
+/**
+ * Created by zaki on 11/9/17.
+ */
+
+public class Server {
+}
---
+++ app/src/main/java/com/example/zaki/smarttubbles/app/AppController.java
@@ -0,0 +1,8 @@
+package com.example.zaki.smarttubbles.app;
+
+/**
+ * Created by zaki on 11/9/17.
+ */
+
+public class AppController {
+}
